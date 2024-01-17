e576498ca641f971576d8a4777e1bcd299d66087
S1CH1 <127799851+S1CH1@users.noreply.github.com>
2024-01-17 16:12:06 +0100

    Ip en fonction de la taille des log.py

--- Ip en fonction de la taille des log.py
+++ Ip en fonction de la taille des log.py
@@ -1,5 +1,5 @@
 tab = []
-def ip_taille(log_file_path = "Path File", taille = 28):
+def ip_taille(log_file_path = "Path File", taille = 'À remplacer par un nombre'):
     with open(log_file_path, 'r') as file:
         for line in file:
             parts = line.split()
