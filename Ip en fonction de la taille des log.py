90c05dafa5139a01d533d523eddf52abda2cb046
S1CH1 <127799851+S1CH1@users.noreply.github.com>
2024-01-17 16:10:59 +0100

    Create Ip en fonction de la taille des log.py

---
+++ Ip en fonction de la taille des log.py
@@ -0,0 +1,12 @@
+tab = []
+def ip_taille(log_file_path = "Path File", taille = 28):
+    with open(log_file_path, 'r') as file:
+        for line in file:
+            parts = line.split()
+            if len(parts) == taille: 
+                if not parts[0] in tab:
+                    tab.append(parts[0])
+                else : 
+                    pass
+    print(tab)
+ip_taille()
